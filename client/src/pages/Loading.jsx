53ea84a63e30dd5f7cfe1673ae9a3f3ac6a35d13
Durgesh Shukla <thegdshukla@gmail.com>
2025-09-13 22:39:37 +0530

    Add Community, Credits, Loading, and Login components with functionality and styling(from prev project)

---
+++ client/src/pages/Loading.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+
+
+function Loading() {
+
+  const navigate = useNavigate();
+  const {fetchUser} = useAppContext();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      fetchUser();
+      navigate("/");
+    }, 8000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-b from-[#242124] to-[#000000] backdrop-opacity-50 flex items-center justify-center h-screen w-screen text-white text-2xl">
+      <div className="w-10 h-10 rounded-full border-4 border-white border-t-transparent animate-spin">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Loading
